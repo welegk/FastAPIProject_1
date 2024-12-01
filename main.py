e15ed98bdf6078d32aff6f3f53b7b3690b6267b0
welegk <leogerte3@gmail.com>
2024-12-01 20:16:20 +0300

    Add file main

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+
+import uvicorn
+from fastapi import FastAPI, Path
+from pydantic import BaseModel, EmailStr
+from items_views import router as items_router
+
+
+app = FastAPI()
+app.include_router(items_router)
+
+class CreateUser(BaseModel):
+    email: EmailStr
+
+
+@app.get('/')
+def hello_index():
+    return {
+        'message': 'Hello index!'
+    }
+
+@app.get('/hello/')
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {'message': f'Hello {name}!'}
+
+
+@app.post('/users/')
+def create_user(user: CreateUser):
+    return {
+        'message': 'success',
+        'email': user.email,
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
