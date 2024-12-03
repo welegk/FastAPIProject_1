526d2786902a1c6d8fc23878e88244407a7bdf5c
welegk <leogerte3@gmail.com>
2024-12-03 22:36:16 +0300

    Adding crud

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from users import crud
+from fastapi import APIRouter
+
+from users.schemas import CreateUser
+
+
+router = APIRouter(prefix='/users', tags=['Users'])
+
+
+@router.post('/')
+def create_user(user: CreateUser):
+    return crud.create_user(user_in=user)
